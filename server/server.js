47f07feb41c9db99aa2407854c7ecdede30827a9
Jose Velez <josevelezq7@gmail.com>
2019-01-28 15:13:56 -0800

    Add morgan logger middleware and change config connections to mongo

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const historyApiFallback = require('connect-history-api-fallback');
 const mongoose = require('mongoose');
 const path = require('path');
 const webpack = require('webpack');
+const morgan = require('morgan');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
@@ -18,10 +19,12 @@ const port  = process.env.PORT || 8080;
 // ================================================================================================
 
 // Set up Mongoose
-mongoose.connect(isDev ? config.db_dev : config.db);
+mongoose.connect(isDev ? config.db_dev : config.db, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 const app = express();
+
+app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
