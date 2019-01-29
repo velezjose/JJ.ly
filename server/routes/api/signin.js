64451e9c7c6ea28f479695d495a65eaf3e868941
Jose Velez <josevelezq7@gmail.com>
2019-01-28 20:53:16 -0800

    Implement verify and logout routes for user auth

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -8,6 +8,7 @@ const isValidEmail = email => {
 
 module.exports = (app) => {
 
+  // Signing up creates a new user
   app.post('/api/account/signup', (req, res, next) => {
     const { body } = req;
     let { firstName, lastName, email, password } = body;
@@ -50,9 +51,7 @@ module.exports = (app) => {
     }
 
     // Verify email doesn't exist
-    User.find({
-      email
-    }, (err, previousUsers) => {
+    User.find({ email }, (err, previousUsers) => {
       if (err) {
         return res.send({
           success: false,
@@ -73,7 +72,7 @@ module.exports = (app) => {
       newUser.password = newUser.generateHash(password);
       
       // Saving new user
-      newUser.save((err, user) => {
+      newUser.save(err => {
         if (err) {
           return res.send({
             success: false,
@@ -90,4 +89,144 @@ module.exports = (app) => {
   
   });
   
+
+  // Signing in creates a new user session
+  app.post('/api/account/signin', (req, res, next) => {
+    const { body } = req;
+    let { email, password } = body;
+
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.',
+      });
+    }
+
+    email = email.toLowerCase();
+
+    if (!isValidEmail(email)) {
+      return res.send({
+        success: false,
+        message: 'Error: Email must be valid.',
+      });
+    }
+
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.',
+      });
+    }
+
+    User.find({ email }, (err, users) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error.',
+        });
+      }
+
+      if (users.length !== 1) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid email or password.',
+        });
+      }
+
+      const user = users[0];
+      const hashedPasswd = user.password;
+
+      if (!user.validPassword(password, hashedPasswd)) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid email or password.',
+        });
+      }
+
+      // create user session
+      let newUserSession = new UserSession();
+      newUserSession.userId = user._id;
+
+      // save new user session
+      newUserSession.save((err, doc) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error.',
+          });
+        }
+
+        return res.send({
+          success: true,
+          message: 'Valid sign in',
+          token: doc._id,
+        });
+      });
+
+    });
+
+  });
+
+
+  // Verifies in user has a session
+  app.get('/api/account/verify', (req, res, next) => {
+    // Get the token
+    const { query } = req;
+    const { token } = query;
+
+    // Verify the token is one of a kind and not deleted
+    UserSession.find({ _id: token, isDeleted: false }, (err, sessions) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error.'
+        });
+      }
+
+      if (sessions.length !== 1) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid token.'
+        });
+      } else {
+        return res.send({
+          success: true,
+          message: 'Found session.'
+        });
+      }
+    });
+
+  });
+
+
+  // Logs out the user and changes their session's isDeleted property to true
+  app.get('/api/account/logout', (req, res, next) => {
+    // Get the token
+    const { query } = req;
+    const { token } = query;
+
+    // Verify the token is one of a kind and not deleted
+    UserSession.findOneAndUpdate({ _id: token, isDeleted: false }, { $set: { isDeleted: true } }, (err, session) => {
+      if (err !== null) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error.'
+        });
+      }
+
+      if (session === null || typeof session !== 'object') {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid token.'
+        });
+      } else {
+        return res.send({
+          success: true,
+          message: 'Deleted session.'
+        });
+      }
+    });
+
+  });
+
 };
